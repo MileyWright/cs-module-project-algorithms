9e2053b65a64ee78131d0965062d04e3f64beb8a
Miley Wright <dishaliekwright@yahoo.com>
2020-08-12 14:01:22 -0400

    moving zeroes completed

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,8 +4,19 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
-
-    pass
+    newArr = []
+    zero_counter = 0
+    for i in range(len(arr)):           # loop through array
+        if arr[i] != 0:
+            newArr.append(arr[i])       # append to newArr if not zero
+        else:
+            zero_counter += 1           # do a zero counter if zero
+    
+    while zero_counter > 0:             # for every zero counter add a zero
+        newArr.append(0)                # to newArr
+        zero_counter -= 1
+    return newArr
+    
 
 
 if __name__ == '__main__':
