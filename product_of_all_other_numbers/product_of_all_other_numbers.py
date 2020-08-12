24fe5c564b48778b9d257209b174d2bcff3e5eac
Miley Wright <dishaliekwright@yahoo.com>
2020-08-12 14:04:17 -0400

    complete

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -4,8 +4,15 @@ Returns: a List of integers
 '''
 def product_of_all_other_numbers(arr):
     # Your code here
+    placeHolder = [0] * len(arr)              # create placeholder, assign 0 to all indexes
+    total = 1                                 # total to be multiplied 
+    for i in range(len(arr)):
+        total = total * arr[i]                # multiply everything and add to total
 
-    pass
+    for i in range(len(arr)):
+        placeHolder[i] = int(total/arr[i])    # for the spot in placehold divide 
+
+    return placeHolder
 
 
 if __name__ == '__main__':
